d7d51122ecc1007dedb86f4ec6a9744c371a9a75
jithish kumar <97963977+jithishkumar410@users.noreply.github.com>
2024-02-24 09:06:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db=mysql.createConnection(
 )
 
 app.get('/',(req,res)=>{
-    const sql = "SELECT * FROM book";
+    const sql = "SELECT * FROM book ORDER BY date DESC";
     db.query(sql,(err,r)=>{
         if(err) return res.json({mes: "error"});
         return res.json(r);
@@ -23,6 +23,80 @@ app.get('/',(req,res)=>{
     }
     )
 })
+app.get('/sr',(req,res)=>{
+    const sql = "SELECT * FROM book ORDER BY date ASC";
+    db.query(sql,(err,r)=>{
+        if(err) return res.json({mes: "error"});
+        return res.json(r);
+
+    }
+    )
+})
+
+app.post('/usr', (req, res) => {
+    const v = [
+        req.body.em,
+        req.body.pass
+    ];
+    
+    const sql = "SELECT * FROM user WHERE email = ? AND password = ? "; 
+    db.query(sql, v, (err, r) => {
+        if (err) {
+            return res.json({ error: err, status: '400' });
+        } else {
+            return res.json({ re: r, status: '200' });
+        }
+    });
+});
+
+app.post('/usrs', (req, res) => {
+    const v = [
+        req.body.em,
+        req.body.pass
+    ];
+    
+    const sql = "INSERT INTO `user` (email,password) VALUES (?)";
+    db.query(sql, [v], (err, r) => {
+        if (err) {
+            return res.json({ error: err, status: '400' });
+        } else {
+            return res.json({ re: r, status: '200' });
+        }
+    });
+});
+
+app.post('/adms', (req, res) => {
+    const v = [
+        req.body.em,
+        req.body.pass
+    ];
+    
+    const sql = "INSERT INTO `admin` (email,password) VALUES (?)";
+    db.query(sql, [v], (err, r) => {
+        if (err) {
+            return res.json({ error: err, status: '400' });
+        } else {
+            return res.json({ re: r, status: '200' });
+        }
+    });
+});
+
+app.post('/adm', (req, res) => {
+    const v = [
+        req.body.em,
+        req.body.pass
+    ];
+    
+    const sql = "SELECT * FROM admin WHERE email = ? AND password = ? "; 
+    db.query(sql, v, (err, r) => {
+        if (err) {
+            return res.json({ error: err, status: '400' });
+        } else {
+            return res.json({ re: r, status: '200' });
+        }
+    });
+});
+
 app.post('/search', (req, res) => {
     const v = req.body.sea; 
     console.log(v)
@@ -39,27 +113,41 @@ app.post('/search', (req, res) => {
 
 
 
-// app.post('/ser',(req,res)=>{
-//     const sql = "INSERT INTO book (title,subject,author,date) VALUES (?)"
+app.post('/add',(req,res)=>{
+    const sql = "INSERT INTO book (title,subject,author,date) VALUES (?)"
     
-//     const vla = [
-//         req.body.title,
-//         req.body.subject,
-//         req.body.author,
-//         req.body.date,
+    const vla = [
+        req.body.title,
+        req.body.subject,
+        req.body.author,
+        req.body.date,
        
-//     ]
+    ]
     
-//     db.query(sql,[vla],(err,r)=>{
-//         if(err){
-//             return res.json(err)
-//         } 
-//         else{
-//             return res.json(r)
-//         }
+    db.query(sql,[vla],(err,r)=>{
+        if(err){
+            return res.json(err)
+        } 
+        else{
+            return res.json(r)
+        }
     
-//     })
-// })
+    })
+})
+
+app.delete('/del', (req, res) => {
+    const v = req.body.id; 
+   
+    const sql = "DELETE FROM book WHERE id = ?"; 
+    db.query(sql, v, (err, r) => {
+        if (err) {
+            return res.json(err);
+        } else {
+            return res.json(r);
+        }
+    });
+});
+
 app.listen(7000,()=>{
     console.log("running")
     
